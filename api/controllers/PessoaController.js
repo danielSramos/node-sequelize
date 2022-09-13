f1dd5687609e5b604170029b879cb473500e388e
Daniel Ramos <danieldsrce@gmail.com>
2022-09-13 10:56:16 -0300

    Adicionando filtros e novas rotas de consulta

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,10 +1,20 @@
 const database = require('../models');
+const Sequelize = require('sequelize');
 
 class PessoaController {
     static async findAllPeople(req, res) {
         try {
-            const allPeople = await database.Pessoas.findAll();
+            const allPeople = await database.Pessoas.scope('todos').findAll();
             return res.status(200).json(allPeople);
+        } catch (err) {
+            res.status(500).json(err.message);
+        }
+    }
+
+    static async findActivePeople(req, res) {
+        try {
+            const activePeoples = await database.Pessoas.findAll();
+            return res.status(200).json(activePeoples);
         } catch (err) {
             res.status(500).json(err.message);
         }
@@ -117,14 +127,66 @@ class PessoaController {
     }
 
     static async restoreMatricula(req, res) {
-        const { id } = req.params;
+        const { estudanteId, matriculaId } = req.params;
         try {
-            await database.Matriculas.restore({ where: { id: Number(id) } });
+            await database.Matriculas.restore({
+                where: {
+                    id: Number(matriculaId),
+                    estudante_id: Number(estudanteId),
+                }
+            });
             return res.status(200).json({ message: `id ${id} restaurado` });
         } catch (err) {
             res.status(500).json(err.message);
         }
     }
+
+    static async getMatriculas(req, res) {
+        const { estudanteId } = req.params;
+        try {
+            const people = await database.Pessoas.findOne({ where: { id: Number(estudanteId) } });
+            const matriculas = await people.getAulasMatriculadas();
+            return res.status(200).json(matriculas);
+        } catch (err) {
+            res.status(500).json(err.message);
+        }
+    }
+
+    static async getMatriculasByTurmas(req, res) {
+        const { turmaId } = req.params;
+        try {
+            const allMatriculas = await database.Matriculas.findAndCountAll({
+                where: {
+                    turma_id: Number(turmaId),
+                    status: 'confirmado'
+                },
+                limit: 2,
+                order: [['estudante_id', 'DESC']]
+            });
+            return res.status(200).json(allMatriculas);
+        } catch (err) {
+            res.status(500).json(err.message);
+        }
+    }
+
+    static async getTurmasFull(req, res) {
+        const { turmaId } = req.params;
+        const locatacaoTurma = 2;
+        try {
+            const turmasFull = await database.Matriculas.findAndCountAll({
+                where: {
+                    status: 'confirmado',
+                },
+                attributes: ['turma_id'],
+                group: ['turma_id'],
+                having: Sequelize.literal(`count(turma_id) >= ${locatacaoTurma}`),
+            });
+            return res.status(200).json(turmasFull.count);
+        } catch (err) {
+            res.status(500).json(err.message);
+        }
+    }
+    
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,18 +4,22 @@ const PessoaController = require('../controllers/PessoaController.js')
 const router = Router();
 
 //Pessoas
-router.get('/pessoas', PessoaController.findAllPeople);
+router.get('/pessoas', PessoaController.findActivePeople);
+router.get('/pessoas/todos', PessoaController.findAllPeople);
 router.get('/pessoas/:id', PessoaController.findPeopleById);
+router.get('/pessoas/:estudanteId/matricula', PessoaController.getMatriculas);
 router.post('/pessoas', PessoaController.createPeople);
+router.post('/pessoas/:id/restaura', PessoaController.restorePeople);
 router.put('/pessoas/:id', PessoaController.updatePeople);
 router.delete('/pessoas/:id', PessoaController.destroyPeople);
-router.post('/pessoas/:id/restaura', PessoaController.restorePeople);
 
 //Matriculas
 router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.findMatriculaById);
+router.get('/pessoas/matricula/:turmaId/confirmadas', PessoaController.getMatriculasByTurmas);
+router.get('/pessoas/matricula/lotada', PessoaController.getTurmasFull);
 router.post('/pessoas/:estudanteId/matricula', PessoaController.createMatricula);
+router.post('/pessoas/:estudanteId/matricula/:matriulaId/restaura', PessoaController.restoreMatricula);
 router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.updateMatricula);
 router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.destroyMatricula);
-router.post('/pessoas/matricula/:id/restaura', PessoaController.restoreMatricula);
 
 module.exports = router;
